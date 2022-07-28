489c1ec9ed60fa4cb3801c4568be922d47c9e4a7
Genilson <geniilsonfernandes@gmail.com>
2022-07-28 18:36:01 -0300

    configure app and jest

--- src/utils/test/renderWithTheme.js
+++ src/utils/test/renderWithTheme.js
@@ -1,12 +1,7 @@
 import { render } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter } from "react-router-dom";
 import theme from "../../styles/theme";
 
 const renderWithTheme = (children) =>
-  render(
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </BrowserRouter>
-  );
+  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
 export default renderWithTheme;
