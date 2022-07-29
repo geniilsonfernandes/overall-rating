ba9db89975df6df6ab1eac88033123b4cc4276fc
Genilson <geniilsonfernandes@gmail.com>
2022-07-29 11:06:34 -0300

    test(RatingCounter): add test

---
+++ src/components/RatingCounter/test.js
@@ -0,0 +1,18 @@
+import renderWithTheme from "../../utils/test/renderWithTheme";
+import RatingCounter from ".";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("RatingCounter", () => {
+  it("should render Rating correctly and return rating counter", () => {
+    const onRating = jest.fn();
+    renderWithTheme(<RatingCounter onRating={onRating} type="text" />);
+
+    const ratingStars = screen.getByLabelText("rating star").childNodes;
+
+    expect(screen.getByText("Overall rating")).toBeInTheDocument();
+
+    userEvent.click(ratingStars[2]);
+    expect(onRating).toBeCalledWith(3);
+  });
+});
