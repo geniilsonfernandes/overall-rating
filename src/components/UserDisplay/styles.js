d73aff461bc154cddb4f4e4644637588f590f1cc
Genilson <geniilsonfernandes@gmail.com>
2022-07-31 11:41:23 -0300

    styles(userdisplay):add responsive configs

--- src/components/UserDisplay/styles.js
+++ src/components/UserDisplay/styles.js
@@ -1,9 +1,15 @@
 import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 24px;
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    gap: 24px;
+    @media (max-width: ${theme.media.mobile}) {
+      flex-direction: column;
+      gap: 10px;
+    }
+  `}
 `;
 
 export const Photo = styled.img`
@@ -13,6 +19,12 @@ export const Photo = styled.img`
     border-radius: 58px;
     background: #e2e2e2;
     border: 8px solid ${theme.colors.white[100]};
+    @media (max-width: ${theme.media.mobile}) {
+      width: 80px;
+      height: 80px;
+      border-radius: 40px;
+      border: 4px solid ${theme.colors.white[100]};
+    }
   `}
 `;
 export const Content = styled.div``;
@@ -21,6 +33,9 @@ export const Title = styled.h1`
     font-weight: 700;
     font-size: ${theme.fontSizes.large};
     color: ${theme.colors.black[500]};
+    @media (max-width: ${theme.media.mobile}) {
+      text-align: center;
+    }
   `}
 `;
 export const Excerpt = styled.span`
@@ -30,5 +45,8 @@ export const Excerpt = styled.span`
     font-size: ${theme.fontSizes.medium};
     color: ${theme.colors.black[500]};
     opacity: 0.5;
+    @media (max-width: ${theme.media.mobile}) {
+      text-align: center;
+    }
   `}
 `;
